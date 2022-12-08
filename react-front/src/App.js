5421ffdde077a3189345cbdf4323397776169bdc
Dmitrii Oreshkin <71903279+dmitrii-subb@users.noreply.github.com>
2022-12-08 16:55:25 +0300

    Add files via upload

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,53 +1,93 @@
-import styles from './App.module.css';
-import { useState } from 'react';
-import { ClassificationResult } from './components/ClassificationResult'
-import { FileMeta } from './components/FileMeta'
-import { BrowseButton } from './components/BrowseButton'
-import { UploadButton } from './components/UploadButton'
+import styles from "./App.module.css";
+import { useState } from "react";
+import { Header } from "./components/Header";
+import { ClassificationResult } from "./components/ClassificationResult";
+import { FileMeta } from "./components/FileMeta";
+import { Action } from "./components/Action";
+import { UploadButton } from "./components/UploadButton";
+
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const payload = useSelector((state) => state.payload);
+  const key = useSelector((state) => state.key);
+  const type = useSelector((state) => state.type);
+  const result = useSelector((state) => state.result);
+  const error = useSelector((state) => state.error);
 
-  const [file, setFile] = useState()
-  const [result, setResult] = useState('')
+  function uploadData(event) {
+    event.preventDefault();
+    let data = new FormData();
+    data.append("key", key);
+    data.append("type", type);
+    data.append("payload", payload);
+    console.log(data);
 
-  function uploadFile(event) {
-    event.preventDefault()
-    var data = new FormData()
-    data.append('file', file)
-
-    fetch('http://172.20.10.7:8080/upload/', {
-      method: 'POST',
+    fetch("http://172.20.10.7:8080/upload/", {
+      method: "POST",
       body: data,
     })
-    .then((response) => {
-      console.log(response)
-      return response.json()
-    })
-    .then((response) => {
-        console.log(response)
-        let cl = JSON.parse(response.result)
-        setResult(`id: ${cl.id} | category: ${cl.result}`)
-      }
-    )
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((response) => {
+        console.log(response);
+        let cl = JSON.parse(response.result);
+        dispatch({
+          type: "SET_RESULT",
+          payload: `id: ${cl.id} | category: ${cl.result}`,
+        });
+      })
+      .catch((err) => {
+        dispatch({
+          type: "SET_ERROR",
+          payload: `error: ${err}`,
+        });
+      });
   }
 
-  function onFile(event) {
-    console.log(event.target.files[0])
-    setFile(event.target.files[0])
+  function onData(event, type) {
+    if (type === "file") {
+      dispatch({
+        type: "SET_PAYLOAD",
+        payload: event.target.files[0],
+      });
+      return;
+    } else if (type === "text") {
+      dispatch({
+        type: "SET_PAYLOAD",
+        payload: event.target.value,
+      });
+    } else if (type === "key") {
+      dispatch({
+        type: "SET_KEY",
+        payload: event.target.value,
+      });
+    }
   }
 
   return (
     <>
       <form className={styles.form}>
-        <h1>Classificator</h1>
-        <BrowseButton onFile={onFile}/>
-        <UploadButton uploadFile={uploadFile}/>
-        {result !== '' &&
-          <ClassificationResult result={result}/>
-        }
-        {file &&
-          <FileMeta file={file}/>
-        }
+        <Routes>
+          <Route
+            path="/:type"
+            element={
+              <>
+                <Header />
+                <h1>Classificator</h1>
+                <Action onData={onData} />
+                <UploadButton uploadData={uploadData} />
+                {result !== "" && <ClassificationResult />}
+                {payload && type === "file" && <FileMeta />}
+              </>
+            }
+          />
+          <Route path="*" element={<Navigate to="/file" />} />
+        </Routes>
       </form>
     </>
   );
---
+++ react-front/src/components/Action/Action.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import styles from "./Action.module.scss";
+import { useSelector } from "react-redux";
+
+function Action({ onData }) {
+  const type = useSelector((state) => state.type);
+
+  return (
+    <>
+      {type === "file" ? (
+        <>
+          <input
+            type="file"
+            id="file"
+            onChange={(e) => onData(e, type)}
+            accept="image/*"
+          />
+          <div className={styles.addDocumentBtn}>
+            <label className={styles.button} htmlFor="file">
+              <span className="material-icons">attachment</span>
+            </label>
+          </div>
+        </>
+      ) : (
+        <>
+          <textarea
+            className={styles.textarea}
+            rows="8"
+            placeholder="text"
+            onChange={(e) => onData(e, type)}
+          ></textarea>
+        </>
+      )}
+      <input type="text" onChange={(e) => onData(e, "key")} />
+    </>
+  );
+}
+
+export { Action };
